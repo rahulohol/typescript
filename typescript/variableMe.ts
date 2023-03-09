a38937e67b50bdc9ee8d0f7f3f23b58077e90677
Rahul Ohol <nevergiupyou123@gmail.com>
2023-03-09 09:43:44 +0530

    basic types styntax

--- typescript/variableMe.ts
+++
@@ -1,6 +0,0 @@
-let greetings: string = "Hello Rahul"; // syntax to define a variable of type string
-
-console.log(greetings);
-
-
-export {};
---
+++ typescript/variablesAndTypes.ts
@@ -0,0 +1,18 @@
+// string type
+let greetings: string = "Hello Rahul"; // syntax to define a variable of type string
+
+console.log(greetings);
+
+// number type
+let rollNo:number = 7; // floats are also comes in number type. so we can define 7.7 or 7.7777 in number type 
+
+// we can avoid writting type for each time by using toFixed method for e.g.
+ let myRollNo = 7;
+ myRollNo.toFixed();
+
+// boolean type
+ let flag:boolean = true;
+  
+
+
+export {};
